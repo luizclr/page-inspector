bb52079d4cfcced49e3ab859a926bfe992e6fb38
Luiz Claudio <luiz.lclr@gmail.com>
2023-01-02 10:27:00 -0300

    feat: add styled-components

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
+import { Title } from "./app.styles";
 
-const App = (): ReactElement => <div>Page Inspector</div>;
+const App = (): ReactElement => <Title>Page Inspector</Title>;
 
 export default App;
